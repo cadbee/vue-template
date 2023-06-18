29dc7dd81194cac870e98277e9d40633fcc3cc74
cadbee <aschiam.net@gmail.com>
2023-06-18 13:56:59 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {vuetify} from "@/plugins/vuetify"
+import OpenLayersMap from "@/plugins/vue3-openlayers";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(vuetify)
+    .use(OpenLayersMap)
+    .mount('#app')
